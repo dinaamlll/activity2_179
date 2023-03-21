49ef54e781685d2b5ba493873cb4bf6bdbb145b5
dinaamlll <dinamaliaa17@gmail.com>
2023-03-20 18:15:45 -0800

    Menambahkan variabel

--- Examp4_179/Examp4_179.cpp
+++ Examp4_179/Examp4_179.cpp
@@ -10,3 +10,19 @@ float LuasLingkaran(float r) {
 float LuasSegitiga(float a, float t) {
 	return 0.5 * a * t;
 }
+
+int main()
+{
+	float panjang, lebar, jejari, alas, tinggi;
+	int pilihan;
+
+	do {
+		cout << "\n\n===================" << endl;
+		cout << "=========MENU=========" << endl;
+		cout << "====================" << endl;
+		cout << "1. Luas Persegi " << endl;
+		cout << "2. Luas Lingkaran " << endl;
+		cout << "3. Luas Segitiga " << endl;
+		cout << "4. exit " << endl;
+		cout << "Pilihan (1/2/3/4): ";
+		cin >> pilihan;
